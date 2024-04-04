b3e74fa6dcabd5511de9ef3afa68f2e05afcdf7d
Jbarber <judahbarber@gmail.com>
2024-04-03 21:40:04 -0500

    send response to discord via embed

--- commands/utility/fight.js
+++ commands/utility/fight.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -34,21 +34,16 @@ module.exports = {
             let attackers = interaction.options.getInteger('attackers');
             let defenders = interaction.options.getInteger('defenders');
             function calculateAttack(x, y){
-                // Response object to return
-                let response = {
-                    attackers: x,
-                    defenders: y
-                }
 
                 // Running totals of the casualties inflicted by each side
                 let attackerDamage = 0;
                 let defenderDamage = 0;
             
-                // 1d20 roll to see if either side hits a crit(offense) which deals ~90% max damage
+                // 1d20 roll to see if either side hits a crit(damage) which deals ~90% max damage
                 let legendAttack = Math.floor(Math.random() * 20 + 1);
                 let legendDefense = Math.floor(Math.random() * 20 + 1);
             
-                // 1d20 roll to see if either side hits a crit(defense) which reduces the enemy damage to ~10% of their max
+                // 1d20 roll to see if either side hits a crit(armor) which reduces the enemy damage to ~10% of their max
                 let attackerMiracle = Math.floor(Math.random() * 20 + 1);
                 let defenderMiracle = Math.floor(Math.random() * 20 + 1);
             
@@ -59,7 +54,7 @@ module.exports = {
                 // Loop through every attacking troop to see if its "shot" hit or missed
                 for (i = 0; i < x; i++){
             
-                    // Check if attackers crit(offense) and defenders crit(defense), setting accuracy to 50%
+                    // Check if attackers crit(damage) and defenders crit(armor), setting accuracy to 50%
                     if (legendAttack == 20 && defenderMiracle == 20){
                         let shot = Math.random() * 2;
                         if (shot >= 1){
@@ -67,7 +62,7 @@ module.exports = {
                         }
                     }
             
-                    // Check to see if a crit(offense) is applicable, and if so, calculate shots at 90% accuracy
+                    // Check to see if a crit(damage) is applicable, and if so, calculate shots at 90% accuracy
                     else if (legendAttack == 20){
                         let shot = Math.random() * 10;
                         if (shot >= 1){
@@ -75,7 +70,7 @@ module.exports = {
                         }
                     }
             
-                    // Check if defenders rolled a crit(defense), and if so, calculate shots at 10% accuracy
+                    // Check if defenders rolled a crit(armor), and if so, calculate shots at 10% accuracy
                     else if (defenderMiracle == 20){
                         let shot = Math.random() * 10;
                         if (shot >= 9){
@@ -95,7 +90,7 @@ module.exports = {
                 // Loop through the defending troops, doing the same thing
                 for (i = 0; i < y; i++){
             
-                    // Offset crits check
+                    // Offsetting crits check
                     if (legendDefense == 20 && attackerMiracle == 20){
                         let shot = Math.random() * 2;
                         if (shot >= 1){
@@ -103,7 +98,7 @@ module.exports = {
                         }
                     }
             
-                    // Crit(offense) check
+                    // Crit(damage) check
                     else if (legendDefense == 20){
                         let shot = Math.random() * 10;
                         if (shot >= 1){
@@ -111,7 +106,7 @@ module.exports = {
                         }
                     }
             
-                    // Attackers crit(defense) check
+                    // Attackers crit(armor) check
                     else if (attackerMiracle == 20){
                         let shot = Math.random() * 10;
                         if (shot >= 9){
@@ -129,23 +124,64 @@ module.exports = {
                 }
             
                 // Calculate remaining troops by subtracting damage
-                response.attackers -= defenderDamage;
-                response.defenders -= attackerDamage;
+                x -= defenderDamage;
+                y -= attackerDamage;
                 
                 // Ensure remaining totals aren't displayed as a negative value
-                if (response.attackers < 0){
-                    response.attackers = 0;
+                if (x < 0){
+                    x = 0;
                 }
-                if (response.defenders < 0){
-                    response.defenders = 0;
+                if (y < 0){
+                    y = 0;
                 }
-            
+
+                 // Response object to return
+                 let response = {
+                    attackers: x,
+                    defenders: y,
+                    attackerCrit1: legendAttack,
+                    attackerCrit2: attackerMiracle,
+                    defenderCrit1: legendDefense,
+                    defenderCrit2: defenderMiracle,
+                    attackerDamage: attackerDamage,
+                    defenderDamage: defenderDamage,
+                    rounds: 0,
+                    victor: ''
+                }
+                console.log(attackerSkill)
+                console.log(defenderSkill)
                 //Return response object
                 return response;
             }
             
             let res = calculateAttack(attackers, defenders);
             
-            await interaction.reply(`${res.attackers} and ${res.defenders}`);
+            // await interaction.reply(`${res.attackers} and ${res.defenders}`);
+            const exampleEmbed = new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setTitle(`Jean Lafitte's Virginia Memorial Battle Simulator`)
+                .setImage('https://upload.wikimedia.org/wikipedia/commons/3/35/Battle_of_New_Orleans.jpg')
+                .setDescription(`"Mama ain't raised no wuss" -Jean Lafitte, 1815`)
+                .addFields(
+                    { name: '\u200B', value: '\u200B' },
+                    { name: `Attacker crit(damage) roll`, value: `${res.attackerCrit1}`, inline: true },
+                    { name: `Defender crit(damage) roll`, value: `${res.defenderCrit1}`, inline: true },
+                    { name: '\u200B', value: '\u200B' },
+                    { name: `Attacker crit(armor) roll`, value: `${res.attackerCrit2}`, inline: true },
+                    { name: `Defender crit(armor) roll`, value: `${res.defenderCrit2}`, inline: true },
+                    { name: '\u200B', value: '\u200B' },
+                    { name: 'Attacker damage', value: `${res.attackerDamage}`, inline: true },
+                    { name: 'Defender damage', value: `${res.defenderDamage}`, inline: true },
+                    { name: '\u200B', value: '\u200B' },
+                    { name: 'Attackers remaining', value: `${res.attackers}`, inline: true },
+                    { name: 'Defenders remaining', value: `${res.defenders}`, inline: true },
+                    { name: '\u200B', value: '\u200B' },
+                    { name: 'Rounds fought', value: 'Some value here', inline: true },
+                    { name: 'Victor', value: 'Some value here', inline: true },
+                    { name: '\u200B', value: '\u200B' }
+                )
+                .setTimestamp();
+
+            interaction.channel.send({ embeds: [exampleEmbed] });
         },
 }
